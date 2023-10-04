4fb56c6e3f79402118c03ee01c8233f11bf3149f
Jhon Santos <kennedyalves871@gmail.com>
2023-10-04 15:00:20 -0300

    Projeto: a Seção de Saudação e Previsão do Tempo

--- main.js
+++ main.js
@@ -3,6 +3,15 @@
 que corresponde ao seletor ou grupo de seletores especificado.*/
 /*addEventListener -> interface que configura uma função que será chamada
 sempre que o evento especificado for entregue ao destino*/
+
+// abrir e fechar o menu
 document.querySelector("#open-nav-menu").addEventListener("click", function(){
     document.querySelector("header nav .wrapper").classList.add("nav-open");
 });
+
+// area de previsão do tempo
+const greetingText = "Good morning!";
+const weatherCondition = "Sunny";
+const userLocatiosn = "New Yourk";
+let temperature = 22.8673;
+let weatherText = "The weather is cloudy in London and it's 22°C outside.";
