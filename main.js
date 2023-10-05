8796b7f3736d894e644e4a17bbf5c0a22516bd6e
Jhon Santos <kennedyalves871@gmail.com>
2023-10-05 09:00:41 -0300

    Os Métodos SetTimeout e SetInterval

--- main.js
+++ main.js
@@ -39,12 +39,9 @@ document.querySelector(".weather-group").addEventListener("click", function(e){
     }
 });
 
-// new Date().getHours();
-// new Date().getMinutes();
-// new Date().getSeconds();
-
-let localTime = new Date();
-
-document.querySelector("span[data-time=hours]").textContent = localTime.getHours();
-document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes();
-document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds();
+setInterval(function(){
+    let localTime = new Date();
+    document.querySelector("span[data-time=hours]").textContent = localTime.getHours();
+    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes();
+    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds();
+},1000);
